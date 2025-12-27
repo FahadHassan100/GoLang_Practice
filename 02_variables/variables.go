0a8a14299ee515d92728077be775a2ed54a6392b
Fahad Hassan <fahadhassan921@gmail.com>
2025-12-27 11:06:15 +0400

    learned hello world and variables

---
+++ 02_variables/variables.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name string = "GoLang"
+	// fmt.Println(name)
+
+	// var check = "Golang"
+	// check = 123
+
+	// fmt.Println(check)
+
+	// var phone int = 123456
+
+	// fmt.Println(phone)
+
+	//Storthand syntax
+	// name := "GoLang"
+	// fmt.Println(name)
+
+	var name string
+
+	name = "GoLang"
+
+	fmt.Println(name)
+}
